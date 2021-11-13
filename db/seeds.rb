ade8917c93a59965fe28a0cdad228e438ba2948b
Carlos Hernandez Topete <chtopete@gmail.com>
2021-11-13 11:40:58 +0900

    a whole new verision, new seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,9 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 require 'open-uri'
+
 Movie.destroy_all
-# List.destroy_all
+List.destroy_all
 
 # the Le Wagon copy of the API
 url = 'http://tmdb.lewagon.com/movie/top_rated'
