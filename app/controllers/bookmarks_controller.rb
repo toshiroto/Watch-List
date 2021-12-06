76c28ca40c345145950af28421514e53c852ae9e
Carlos Hernandez Topete <chtopete@gmail.com>
2021-12-06 14:03:02 +0900

    uncommited changes

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -5,7 +5,7 @@ class BookmarksController < ApplicationController
   end
 
   def create
-    @list = List.find(params[:id])
+    @list = List.find(params[:list_id])
     @bookmark = Bookmark.new(bookmark_params)
     @bookmark.list = @list
     if @bookmark.save
